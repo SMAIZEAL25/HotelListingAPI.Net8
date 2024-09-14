cff20b7ce2d22fde34a6d21f8bb5356f550b6aa0
smaizeal25 <samizeal25@gmail.com>
2024-09-14 10:52:55 +0100

    DTO ADDED

---
+++ HotelListingAPI/HotelDTO/HotelDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelListingAPI.HotelDTO
+{
+    public class HotelDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+    }
+}
